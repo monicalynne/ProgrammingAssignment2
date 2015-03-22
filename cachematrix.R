627796c21794cf6123adeaf048513e9d98931af5
monicalynne <monica.kriete@gmail.com>
2015-03-21 22:40:02 -0400

    Completed Programming Assignment #2 for week 3 of R Programming.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix() takes a matrix and stores it to an object 'x' within the environment of the function,
+## then creates an object 'm' where the inverse of the matrix can be stored and retrieved.
 
-## Write a short comment describing this function
+makeCacheMatrix <- function(x = matrix()) {             
+  m <- NULL                                
+  set <- function(y) {                          ## set() is a function within makeCacheMatrix()
+    x <<- y                                     ## stores the input to set() as an object 'x'
+    m <<- NULL                                  ## creates an empty object 'm'
+    
+    get <- function() x                           ## get() returns 'x'
+    setinverse <- function(solve) m <<- solve     ## setinverse() uses solve() to return the inverse of matrix 'x' to 'm'
+    getinverse <- function() m                    ## getinverse() returns 'm'
+    
+    list(set = set, get = get,                    ## makeCacheMatrix() returns a list containing the values of set, get, setinverse, and getinverse
+         setinverse = setinverse,              
+         getinverse = getinverse)
+  }
 
-makeCacheMatrix <- function(x = matrix()) {
 
-}
+## cacheSolve() looks to see if the solution to a matrix has been cached with makeCacheMatrix() and 
+## either returns the cached solution, or solves and caches the input matrix for future use
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) {                 
+  m <- x$getinverse()                                ## runs getinverse()  
+  if(!is.null(m)) {                                  ## looks up & returns stored inverse of matrix
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()                             
+  m <- solve(data, ...)                             ## solves matrix
+  x$setinverse(m)                                   ## caches inverse of matrix for future use
+  m                                                 ## returns inverse of matrix 
 }
